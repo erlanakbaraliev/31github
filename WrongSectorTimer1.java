c9c26c6bda0d8b533d4287dd08f7dbc9fb481ec5
Erlan Akbaraliev <104745090+erlanakbaraliev@users.noreply.github.com>
2023-11-22 22:07:15 +0100

    Structuraltest

---
+++ WrongSectorTimer1.java
@@ -0,0 +1,9 @@
+package Task_2.race.car;
+
+public class WrongSectorTimer1 {
+    public int[] arr = {1,2,3};
+
+    public static void WrongSectorTimer(int[] arr) {
+        this.arr = arr;
+    }
+}
